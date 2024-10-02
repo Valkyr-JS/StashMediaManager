85e1a52ea5420a2edd1229c57167e72168f43c82
ValkyrJS <valkyr.photos@gmail.com>
2024-10-02 16:13:58 +0100

    Rework to make queries "Stash-first"

--- apis/aylo/aylo-json-to-meta-stash.ps1
+++ apis/aylo/aylo-json-to-meta-stash.ps1
@@ -77,141 +77,89 @@ function Set-AyloJsonToMetaStash {
     else { Write-Host "Backup will not be created." }
 
     $dataDir = Join-Path $userConfig.general.scrapedDataDirectory "aylo"
+    $actorsDataDir = Join-Path $dataDir "actor"
+    $scenesDataDir = Join-Path $dataDir "scene"
 
-    # ---------------------------------------------------------------------------- #
-    #                                  PERFORMERS                                  #
-    # ---------------------------------------------------------------------------- #
-    
-    $actorsDir = Join-Path $dataDir "actor"
-    $actorsData = Get-ChildItem $actorsDir -Filter "*.json"
-
-    # Loop through each set of actor data
-    foreach ($actor in $actorsData) {
-        $actor = Get-Content $actor -raw | ConvertFrom-Json
-
-        # ------------------------------ Performer tags ------------------------------ #
-
-        [array]$newTags = $actor.tags
-        [array]$newParentNames = Get-ParentTagsFromTagsList -tagList $actor.tags
-
-        # Create new parent tags if they don't already exist
-        if ($newParentNames.Count) { $null = Set-ParentTagsFromTagNameList -tagList $newParentNames }
-
-        # Create new tags if they don't already exist
-        if ($newTags.Count) { $null = Set-TagsFromTagList -tagList $newTags }
-
-        # -------------------------------- Performers -------------------------------- #
-
-        # Query Stash to see if the performer exists. Disambiguation is the
-        # performer ID, which we use to query.
-        $existingActor = Get-StashPerformerByDisambiguation $actor.id
-
-        # If no data is found, create the new performer
-        if ($existingActor.data.findPerformers.performers.count -eq 0) {
-
-            # Format alias list
-            [array]$alias_list = @()
-            if ($actor.aliases.count -gt 0) {
-                foreach ($alias in $aliases) {
-                    # Add each valid alias to the list
-                    if ($alias.Trim().Length -gt 0) {
-                        $alias_list += "$($alias.Trim())"
-                    }
-                }
-            }
-
-            # Format gender
-            $gender = $actor.gender
-            if ($gender -eq "trans") { $gender = "TRANSGENDER_FEMALE" }
-            $gender = $gender.ToUpper()
-
-            # Format measurements / penis length - value is mostly gender
-            # dependent but this is inconsistent.
-            $measurements = $null
-            $penis_length = $null
-            $measurementsAsPlength = $gender -like "FEMALE" -or $measurements -match "-"
-            if ($actor.measurements) {
-                if ($measurementsAsPlength) {
-                    $measurements = $actor.measurements
-                }
-                else {
-                    # Remove any unit from the string
-                    $penis_length = $actor.measurements -replace "[^0-9]", ""
-
-                    # Check if the value is a number, and if not don't use it
-                    if ($penis_length) {
-                        # Convert inches to cm
-                        $penis_length = Get-InchesToCm ([Int]$penis_length)
-                    }
-                }
-            }
-
-            # Get tags
-            $actorTagIDs = @()
-            foreach ($tagID in $actor.tags.id) {
-                $result = Get-StashTagByAlias -alias "aylo-$tagID"
-                $actorTagIDs += $result.data.findTags.tags.id
-            }
-
-            $null = Set-StashPerformer -disambiguation $actor.id -name $actor.name -gender $gender -alias_list $alias_list -birthdate $actor.birthday -details $actor.bio -height_cm ([math]::Round((Get-InchesToCm $actor.height))) -image $actor.images.profile."0".lg.url -measurements $measurements -penis_length $penis_length -weight ([math]::Round((Get-LbsToKilos $actor.weight))) -tag_ids $actorTagIDs
-        }
-    }
+    # Logging meta
+    $metaScenesUpdated = 0
 
     # ---------------------------------------------------------------------------- #
     #                                    Scenes                                    #
     # ---------------------------------------------------------------------------- #
 
+    # Fetch all Stash scenes not marked as organized
+    $StashGQL_Query = 'query FindUnorganizedScenes($filter: FindFilterType, $scene_filter: SceneFilterType) {
+        findScenes(filter: $filter, scene_filter: $scene_filter) {
+            scenes {
+                files { path }
+                id
+                title
+            }
+        }
+    }'
+    $StashGQL_QueryVariables = '{
+        "filter": { "per_page": -1 },
+        "scene_filter": {
+            "organized": false,
+            "path": {
+                "value": "/scene/",
+                "modifier": "MATCHES_REGEX"
+            }
+        }
+    }' 
 
-    $scenesDir = Join-Path $dataDir "scene"
-    $scenesData = Get-ChildItem -Path $scenesDir -Recurse -File -Filter "*.json"
+    $result = Invoke-StashGQLQuery -query $StashGQL_Query -variables $StashGQL_QueryVariables
+    $stashScenesToProcess = [array]$result.data.findScenes.scenes
 
-    foreach ($scene in $scenesData) {
-        $scene = Get-Content $scene -raw | ConvertFrom-Json
-        Write-Host `n"Starting update for $($scene.title)" -ForegroundColor Cyan
+    foreach ($stashScene in $stashScenesToProcess) {
+        Write-Host "Updating Stash scene $($stashScene.id)" -ForegroundColor Cyan
 
-        # -------------------------------- Scene tags -------------------------------- #
+        # Get the associated data file
+        $ayloID = $stashScene.files.path.split("/")[5].split(" ")[0]
+        $sceneData = Get-ChildItem -Path $scenesDataDir -Recurse -File -Filter "*.json" | Where-Object { $_.BaseName -match "^$ayloID\s" }
 
-        [array]$newTags = $scene.tags
-        [array]$newParentNames = Get-ParentTagsFromTagsList -tagList $scene.tags
-
-        # Create new parent tags if they don't already exist
-        if ($newParentNames.Count) { $null = Set-ParentTagsFromTagNameList -tagList $newParentNames }
-
-        # Create new tags if they don't already exist
-        if ($newTags.Count) { $null = Set-TagsFromTagList -tagList $newTags }
-
-        # ---------------------------------- Scenes ---------------------------------- #
-
-        # Query Stash to see if the scene already exists and data has been added.
-        # $existingScene = Get-StashSceneByCode $scene.id
-
-        # If no data is found, query Stash to see if the scene exists but hasn't
-        # had data added yet.
-        # if ($existingScene.data.findScenes.scenes.count -eq 0) {
-        $existingScene = Get-StashSceneByIdInPath $scene.id
-        if ($existingScene.data.findScenes.scenes.count -eq 0) {
-            Write-Host "Scene $($scene.title) not found in Stash." -ForegroundColor Yellow
+        if (!($sceneData)) {
+            Write-Host "FAILED: No data file found that matches Stash scene $($stashScene.id)." -ForegroundColor Red
         }
         else {
-            # Get performers
+            $sceneData = Get-Content $sceneData -raw | ConvertFrom-Json
+
+            # -------------------------------- Performers -------------------------------- #
+
+            # Create any performers that aren't in Stash yet
+            $null = Set-PerformersFromActorList -actorsDataDir $actorsDataDir -actorList $sceneData.actors
+
+            # Fetch all performer IDs from Stash
             $performerIDs = @()
-            foreach ($id in $scene.actors.id) {
+            foreach ($id in $sceneData.actors.id) {
                 $result = Get-StashPerformerByDisambiguation -disambiguation $id
                 $performerIDs += $result.data.findPerformers.performers.id
             }
 
-            # Get tags
+            # ----------------------------------- Tags ----------------------------------- #
+
+            # Create any parent tags that aren't in Stash yet
+            [array]$parentTagNames = Get-ParentTagsFromTagsList -tagList $sceneData.tags
+            if ($parentTagNames.Count) { $null = Set-ParentTagsFromTagNameList -tagList $parentTagNames }
+
+            # Create new tags that aren't in Stash yet
+            if ($sceneData.tags.Count) { $null = Set-TagsFromTagList -tagList $sceneData.tags }
+    
+            # Fetch all tag IDs from Stash
             $tagIDs = @()
-            foreach ($id in $scene.tags.id) {
+            foreach ($id in $sceneData.tags.id) {
                 $result = Get-StashTagByAlias -alias "aylo-$id"
                 $tagIDs += $result.data.findTags.tags.id
             }
 
-            # Update the found scene
-            $null = Set-StashSceneUpdate -id $existingScene.data.findScenes.scenes.id -code $scene.id -cover_image $scene.images.poster."0".xx.url -details $scene.description -performer_ids $performerIDs -tag_ids $tagIDs -title $scene.title -date $scene.dateReleased
+            # Update the scene
+            $null = Set-StashSceneUpdate -id $stashScene.id -code $sceneData.id -cover_image $sceneData.images.poster."0".xx.url -details $sceneData.description -performer_ids $performerIDs -tag_ids $tagIDs -title $sceneData.title -date $sceneData.dateReleased
+            $metaScenesUpdated++
         }
     }
-    # }
+
+    Write-Host "All updates complete" -ForegroundColor Cyan
+    Write-Host "Scenes updated: $metaScenesUpdated"
 }
 
 # ---------------------------------------------------------------------------- #
@@ -287,6 +235,92 @@ function Set-TagsFromTagList {
         
                 # Create the tag
                 $null = Set-StashTag -name $tag.name.Trim() -aliases $aliases -parent_ids $parentTagID
+            }
+        }
+    }
+}
+
+# Create missing Stash performers from a list of Aylo actors
+function Set-PerformersFromActorList {
+    param (
+        [Parameter(Mandatory)][String]$actorsDataDir,
+        [Parameter(Mandatory)][array]$actorList
+    )
+
+    foreach ($actor in $actorList) {
+        # Get the associated data file
+        $ayloID = $actor.id
+        $actorData = Get-ChildItem -Path $actorsDataDir -Recurse -File -Filter "*.json" | Where-Object { $_.BaseName -match "^$ayloID\s" }
+        
+        if (!($actorData)) {
+            Write-Host "FAILED: No data file found that matches Aylo performer $ayloID." -ForegroundColor Red
+        }
+        else {
+            $actorData = Get-Content $actorData -raw | ConvertFrom-Json
+
+            # ------------------------------ Performer tags ------------------------------ #
+
+            # Create any parent tags that aren't in Stash yet
+            [array]$parentTagNames = Get-ParentTagsFromTagsList -tagList $actorData.tags
+            if ($parentTagNames.Count) { $null = Set-ParentTagsFromTagNameList -tagList $parentTagNames }
+
+            # Create new tags that aren't in Stash yet
+            if ($actorData.tags.Count) { $null = Set-TagsFromTagList -tagList $actorData.tags }
+
+            # --------------------------------- Performer -------------------------------- #
+
+            # Query Stash to see if the performer exists. Disambiguation is the
+            # performer ID, which we use to query.
+            $existingPerformer = Get-StashPerformerByDisambiguation $actorData.id
+
+            # If no data is found, create the new performer
+            if ($existingPerformer.data.findPerformers.performers.count -eq 0) {
+
+                # Format alias list
+                [array]$alias_list = @()
+                if ($actorData.aliases.count -gt 0) {
+                    foreach ($alias in $aliases) {
+                        # Add each valid alias to the list
+                        if ($alias.Trim().Length -gt 0) {
+                            $alias_list += "$($alias.Trim())"
+                        }
+                    }
+                }
+        
+                # Format gender
+                $gender = $actorData.gender
+                if ($gender -eq "trans") { $gender = "TRANSGENDER_FEMALE" }
+                $gender = $gender.ToUpper()
+        
+                # Format measurements / penis length - value is mostly gender
+                # dependent but this is inconsistent.
+                $measurements = $null
+                $penis_length = $null
+                $measurementsAsPlength = $gender -like "FEMALE" -or $measurements -match "-"
+                if ($actorData.measurements) {
+                    if ($measurementsAsPlength) {
+                        $measurements = $actorData.measurements
+                    }
+                    else {
+                        # Remove any unit from the string
+                        $penis_length = $actorData.measurements -replace "[^0-9]", ""
+        
+                        # Check if the value is a number, and if not don't use it
+                        if ($penis_length) {
+                            # Convert inches to cm
+                            $penis_length = Get-InchesToCm ([Int]$penis_length)
+                        }
+                    }
+                }
+        
+                # Get tags
+                $tagIDs = @()
+                foreach ($id in $actorData.tags.id) {
+                    $result = Get-StashTagByAlias -alias "aylo-$id"
+                    $tagIDs += $result.data.findTags.tags.id
+                }
+            
+                $null = Set-StashPerformer -disambiguation $actorData.id -name $actorData.name -gender $gender -alias_list $alias_list -birthdate $actorData.birthday -details $actorData.bio -height_cm ([math]::Round((Get-InchesToCm $actorData.height))) -image $actorData.images.profile."0".lg.url -measurements $measurements -penis_length $penis_length -weight ([math]::Round((Get-LbsToKilos $actorData.weight))) -tag_ids $tagIDs
             }
         }
     }
