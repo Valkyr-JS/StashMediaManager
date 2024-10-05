8d61e4f02f79c59474b0948303564f427d422c17
ValkyrJS <valkyr.photos@gmail.com>
2024-10-06 00:52:26 +0100

    Download AF scene

--- apis/addfriends/addfriends-downloader.ps1
+++ apis/addfriends/addfriends-downloader.ps1
@@ -7,18 +7,15 @@ function Get-AFSceneAllMedia {
 
     $userConfig = Get-Content $pathToUserConfig -raw | ConvertFrom-Json
     $assetsDir = $userConfig.general.assetsDirectory
-    # $dataDir = $userConfig.general.scrapedDataDirectory
-    # $downloadDir = $userConfig.general.downloadDirectory
-    # $storageDir = $userConfig.general.storageDirectory
+    $downloadDir = $userConfig.general.downloadDirectory
+    $storageDir = $userConfig.general.storageDirectory
     $subDir = Join-Path "addfriends" "video" $slug
-
-    # Get the filename for the poster
-    $posterCdnFilename = $sceneData.file_name.split(".")[0]
-    $posterFilename = Set-MediaFilename -mediaType "scene" -extension "jpg" -id $sceneData.id -title $sceneData.title
     
     Write-Host `n"Downloading all media for scene #$($sceneData.id) - $($sceneData.title)." -ForegroundColor Cyan
 
-    # Poster
+    # POSTER
+    $posterCdnFilename = $sceneData.file_name.split(".")[0]
+    $posterFilename = Set-MediaFilename -mediaType "poster" -extension "jpg" -id $sceneData.id -title $sceneData.title
     $posterUrl = "https://static.addfriends.com/vip/posters/$posterCdnFilename-big.jpg"
     Write-Host $posterUrl
 
@@ -57,5 +54,45 @@ function Get-AFSceneAllMedia {
         Write-Host "Skipping poster as it already exists at $existingPath."
     }
     
-    # Scene
+    # SCENE
+    $filename = Set-MediaFilename -mediaType "scene" -extension "mp4" -id $sceneData.id -title $sceneData.title
+    $subDir = Join-Path "addfriends" "video" $slug
+
+    # Check if the file exists
+    $existingPath = $null
+    foreach ($dir in @($downloadDir, $storageDir)) {
+        $testPath = Join-Path $dir $subDir $filename
+        if (Test-Path -LiteralPath $testPath) { $existingPath = $testPath }
+    }
+
+    # Download if the file doesn't exist
+    if ($null -eq $existingPath) {
+        $outputPath = Join-Path $downloadDir $subDir $filename
+
+        Write-Host "Downloading scene: $outputPath"
+        if (!(Test-Path $outputDir)) { New-Item -ItemType "directory" -Path $outputDir }
+        try {
+            Invoke-WebRequest -uri $sceneData.dl -OutFile ( New-Item -Path $outputPath -Force ) 
+        }
+        catch {
+            Write-Host "ERROR: Could not download scene: $outputPath" -ForegroundColor Red
+            Write-Host "$_" -ForegroundColor Red
+            
+            # If an empty or partial file has been generated, delete it
+            if (Test-Path $outputPath) { Remove-Item $outputPath }
+        }
+
+        # Check the file has been downloaded successfully.
+        # TODO - Check existing file matches db MD5 hash
+        if (!(Test-Path -LiteralPath $outputPath)) {
+            Write-Host "FAILED: File not downloaded." -ForegroundColor Red
+        }
+        else {
+            Write-Host "SUCCESS: Downloaded $outputPath" -ForegroundColor Green
+        }
+
+    }
+    else {
+        Write-Host "Skipping scene as it already exists at $existingPath."
+    }
 }
