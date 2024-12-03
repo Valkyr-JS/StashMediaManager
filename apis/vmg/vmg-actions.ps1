d9b11b4ef36cc331640d97e5ce675e51a37fd990
ValkyrJS <valkyr.photos@gmail.com>
2024-12-03 00:29:13 +0000

    Get-VMGAllContentByModelSlug all json before all scenes

--- apis/vmg/vmg-actions.ps1
+++ apis/vmg/vmg-actions.ps1
@@ -7,20 +7,25 @@ function Get-VMGAllContentByModelSlug {
     )
     $sceneIDs = Get-VMGSceneIDsByModelSlug -modelSlug $modelSlug -pathToUserConfig $pathToUserConfig -site $site
     $sceneIndex = 1
-
-    # TODO - Rework to scrape all scenes first, then download the content.
-    # Rather than scrape a scene then download immediately after. 
+    
+    # Get the JSON for each scene first
+    $sceneJSONs = @()
     foreach ($sceneID in $sceneIDs) {
         Write-Host `n"Scene $sceneIndex/$($sceneIDs.Length)" -Foreground Cyan
         $pathToSceneJson = Get-VMGAllJson -pathToUserConfig $pathToUserConfig -sceneID $sceneID
         if (($null -ne $pathToSceneJson) -and !(Test-Path -LiteralPath $pathToSceneJson)) {
             Write-Host `n"ERROR: scene $sceneID JSON data not found - $pathToSceneJson." -ForegroundColor Red
         }
-        elseif ($null -ne $pathToSceneJson) {
-            $sceneData = Get-Content -LiteralPath $pathToSceneJson -raw | ConvertFrom-Json
-            $sceneData = $sceneData.data.findOneVideo
-            Get-VMGSceneAllMedia -sceneData $sceneData -pathToUserConfig $pathToUserConfig
-        }
+        else { $sceneJSONs += $pathToSceneJson }
+        $sceneIndex++
+    }
+
+    # Scrape the data for the scenes
+    $sceneIndex = 1
+    foreach ($json in $sceneJSONs) {
+        $sceneData = Get-Content -LiteralPath $pathToSceneJson -raw | ConvertFrom-Json
+        $sceneData = $sceneData.data.findOneVideo
+        Get-VMGSceneAllMedia -sceneData $sceneData -pathToUserConfig $pathToUserConfig
         $sceneIndex++
     }
 }
